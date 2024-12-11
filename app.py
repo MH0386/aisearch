443f54f136c526a76f1c8dfd294368f32cfc4687
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-12-11 23:22:31 +0200

    Refactor application agents and update dependencies in pyproject.toml

--- app.py
+++ app.py
@@ -1,35 +1,19 @@
 from phi.agent import Agent
-from phi.model.openai import OpenAIChat
+from phi.model.groq import Groq
+from phi.playground import Playground, serve_playground_app
 from phi.storage.agent.sqlite import SqlAgentStorage
 from phi.tools.duckduckgo import DuckDuckGo
-from phi.tools.yfinance import YFinanceTools
-from phi.playground import Playground, serve_playground_app
 
-web_agent = Agent(
-    name="Web Agent",
-    model=OpenAIChat(id="gpt-4o"),
+agent = Agent(
+    name="Llama3",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    storage=SqlAgentStorage(table_name="llama3", db_file="agents.db"),
+    add_history_to_messages=True,
     tools=[DuckDuckGo()],
-    instructions=["Always include sources"],
-    storage=SqlAgentStorage(table_name="web_agent", db_file="agents.db"),
-    add_history_to_messages=True,
     markdown=True,
 )
 
-finance_agent = Agent(
-    name="Finance Agent",
-    model=OpenAIChat(id="gpt-4o"),
-    tools=[
-        YFinanceTools(
-            stock_price=True, analyst_recommendations=True, company_info=True, company_news=True
-        )
-    ],
-    instructions=["Use tables to display data"],
-    storage=SqlAgentStorage(table_name="finance_agent", db_file="agents.db"),
-    add_history_to_messages=True,
-    markdown=True,
-)
-
-app = Playground(agents=[finance_agent, web_agent]).get_app()
+app = Playground(agents=[ agent]).get_app()
 
 if __name__ == "__main__":
-    serve_playground_app("playground:app", reload=True)
+    serve_playground_app(app="playground:app", reload=True)
